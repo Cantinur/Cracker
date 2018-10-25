eff8b270b3a5ffcd8fde3725860ccda1e9cdeec1
Cantinur <henrik.a.sandberg@icloud.com>
2018-10-25 12:17:14 +0200

    Linux test

--- main.c
+++ main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-
-void brutForce()
+void brutForce(char hash[100])
 {
+    char check[100] = "";
+    
+    while(){
+        //Finner hash
+    }
+    //Riktig hash
+
+
     printf("Hello Wordl");
 }
 
 int main(int argc, char const *argv[])
 {
     /* code */
+
+
     return 0;
 }
